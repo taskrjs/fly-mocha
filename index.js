83515c842505cb375cc3197c0f1e30cf619e745f
Jorge Bucaran <jbucaran@me.com>
2015-07-11 14:34:52 +0900

    update for fly-util 2.0

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ module.exports = function () {
   this.mocha = function (opts) {
     const mocha = new Mocha(opts)
     return new Promise(function (resolve, reject) {
-      this.unwrap().then((files) => {
-        files.forEach(_ => mocha.addFile(_.file))
+      this.unwrap((files) => {
+        files.forEach(file => mocha.addFile(file))
         mocha.run((failures) => failures > 0
           ? reject(failures + " error(s).")
           : resolve())
